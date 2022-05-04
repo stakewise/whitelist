4821a13b9c7ca5ab5ce4bae8246cd9c53d1f943b
cyc60 <avsysoev60@gmail.com>
2022-05-04 12:03:27 +0300

    Add eth address validation

--- app/main.py
+++ app/main.py
@@ -1,17 +1,32 @@
+import binascii
 import typing
 
+from eth_utils import decode_hex
 from fastapi import BackgroundTasks, Depends, FastAPI, Header, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from web3 import Web3
 
 from app import config
 
-from .tasks import update_whitelist
+from .tasks import check_whitelist, update_whitelist
 
+w3 = Web3()
 app = FastAPI()
 
 
 class AddressRequest(BaseModel):
     address: str
+
+    @validator("address")
+    def address_validation(cls, value):
+        if len(value) != 42 or not value.startswith("0x"):
+            raise ValueError("Invalid ETH Address")
+        if not Web3.isChecksumAddress(value):
+            raise ValueError("Invalid ETH Address")
+        try:
+            decode_hex(value)
+        except (binascii.Error, TypeError):
+            raise ValueError("Invalid ETH Address")
 
 
 class AuthorizerDependency:
@@ -42,3 +57,12 @@ async def whitelist_delete(
     address = address_request.address
     background_tasks.add_task(update_whitelist, address, False)
     return {"status": "success"}
+
+
+@app.get("/service/whitelist", dependencies=[Depends(AuthorizerDependency())])
+async def whitelist_get(address: str):
+    try:
+        AddressRequest.address_validation(address)
+    except ValueError as e:
+        return {"error": str(e)}
+    return {"result": check_whitelist(address)}
